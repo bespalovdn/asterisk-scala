7a6d1acc5cf3d5d9bb652e55038c9da5b585609f
Dmitry Bespalov <bespalovdn@gmail.com>
2015-04-04 15:12:46 +0300

    GetVariableCommand refactoring

--- agi/src/main/scala/com/github/bespalovdn/asteriskscala/agi/command/GetVariableCommand.scala
+++ agi/src/main/scala/com/github/bespalovdn/asteriskscala/agi/command/GetVariableCommand.scala
@@ -2,25 +2,25 @@ package com.github.bespalovdn.asteriskscala.agi.command
 
 import com.github.bespalovdn.asteriskscala.agi.execution.AsyncActionSupport
 import com.github.bespalovdn.asteriskscala.agi.handler.AgiCommandSender
-import com.github.bespalovdn.asteriskscala.agi.response.{FailResponse, GetVariableResult}
+import com.github.bespalovdn.asteriskscala.agi.response.{AgiResponse, FailResponse, GetVariableResult}
 
 import scala.concurrent.Future
 
 class GetVariableCommand(variable: String) extends AgiCommand with AsyncActionSupport
 {
-    override def send()(implicit sender: AgiCommandSender): Future[GetVariableResult] = for(
-        ar <- sender.send(this);
-        result <- ar.response match {
-            case "200 result=0" => GetVariableResult.NotSet.toFuture
-            case GetVariableCommand.regex(value) => GetVariableResult.Success(value).toFuture
-            case other => Future.failed(FailResponse.Error(other))
-        }
-    ) yield result
+    override def send()(implicit sender: AgiCommandSender): Future[GetVariableResult] =
+        sender.send(this) >>= toResult
 
+    private def toResult(ar: AgiResponse): Future[GetVariableResult] = ar.response match {
+        case GetVariableCommand.notSet => GetVariableResult.NotSet.toFuture
+        case GetVariableCommand.success(value) => GetVariableResult.Success(value).toFuture
+        case other => Future.failed(FailResponse.Error(other))
+    }
 
 }
 
 object GetVariableCommand
 {
-    private lazy val regex = """200 result=1 \((.*)\)""".r
+    private lazy val notSet  = """200 result=0"""
+    private lazy val success = """200 result=1 \((.*)\)""".r
 }
