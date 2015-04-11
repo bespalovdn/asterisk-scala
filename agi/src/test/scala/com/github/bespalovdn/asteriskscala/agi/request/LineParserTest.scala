10d3e556e1e365ac68a38d86020ac05775be29fa
Dmitry Bespalov <bespalovdn@gmail.com>
2015-04-11 21:46:35 +0300

    trying to run ScalaTest in Gradle - still with no success.

--- agi/src/test/scala/com/github/bespalovdn/asteriskscala/agi/request/LineParserTest.scala
+++ agi/src/test/scala/com/github/bespalovdn/asteriskscala/agi/request/LineParserTest.scala
@@ -1,7 +1,10 @@
 package com.github.bespalovdn.asteriskscala.agi.request
 
 import com.github.bespalovdn.asteriskscala.common.test.TestSupport
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 
+@RunWith(classOf[JUnitRunner])
 class LineParserTest extends TestSupport
 {
     "LineParserTest" should
