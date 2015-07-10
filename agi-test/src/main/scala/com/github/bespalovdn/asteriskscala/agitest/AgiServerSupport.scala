e5b34c2283e3e6f6dfb7602aa095ff58a6185581
Dmitry Bespalov <bespalovdn@gmail.com>
2015-07-10 20:39:04 +0300

    AgiServerSupport impl

--- agi-test/src/main/scala/com/github/bespalovdn/asteriskscala/agitest/AgiServerSupport.scala
+++ agi-test/src/main/scala/com/github/bespalovdn/asteriskscala/agitest/AgiServerSupport.scala
@@ -1,5 +1,26 @@
 package com.github.bespalovdn.asteriskscala.agitest
 
-class AgiServerSupport {
+import java.net.InetSocketAddress
 
+import akka.actor.ActorRef
+import com.github.bespalovdn.asteriskscala.agi.AgiServerActor
+import com.github.bespalovdn.asteriskscala.agi.handler.AgiRequestHandlerFactory
+import com.github.bespalovdn.asteriskscala.common.test.TestSupportAkka
+
+trait AgiServerSupport extends TestSupportAkka
+{
+    var agiServer: ActorRef = null
+
+    def agiAddr: InetSocketAddress
+    def agiHandlerFactory: AgiRequestHandlerFactory
+
+    override protected def beforeEach() {
+        super.beforeEach()
+        agiServer = system.newActor("agi-server"){new AgiServerActor(agiAddr, agiHandlerFactory)}
+    }
+
+    override protected def afterEach(): Unit = {
+        system.stop(agiServer)
+        super.afterEach()
+    }
 }
