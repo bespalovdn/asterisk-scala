ee97c1730dd4e33b2b5b5c2c33e393c42833f736
Dmitry Bespalov <bespalovdn@gmail.com>
2016-04-03 21:44:30 +0300

    update sbt.version

--- project/Build.scala
+++ project/Build.scala
@@ -22,12 +22,12 @@ object Build extends sbt.Build
 
     private object dependency {
         lazy val akka = Seq(
-            "com.typesafe.akka" % "akka-actor_2.10" % "2.3.4",
-            "com.typesafe.akka" % "akka-slf4j_2.10" % "2.3.4",
-            "com.typesafe.akka" % "akka-testkit_2.10" % "2.3.4")
+            "com.typesafe.akka" %% "akka-actor" % "2.3.4",
+            "com.typesafe.akka" %% "akka-slf4j" % "2.3.4",
+            "com.typesafe.akka" %% "akka-testkit" % "2.3.4")
         lazy val netty = "io.netty" % "netty-all" % "4.0.13.Final"
-        lazy val scalaArm = "com.jsuereth" % "scala-arm_2.10" % "1.4"
-        lazy val scalaTest = "org.scalatest" % "scalatest_2.10" % "2.2.1"
+        lazy val scalaArm = "com.jsuereth" %% "scala-arm" % "1.4"
+        lazy val scalaTest = "org.scalatest" %% "scalatest" % "2.2.5"
         lazy val log = Seq(
             "org.slf4j" % "slf4j-api" % "1.7.12",
             "com.github.bespalovdn" % "scala-log_2.10" % "1.0-SNAPSHOT",
