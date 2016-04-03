5ff7d8e04258a45fd0fbc7c3fb1d388ef92d998b
Dmitry Bespalov <bespalovdn@gmail.com>
2016-04-03 20:30:57 +0300

    build settings changed

--- project/Build.scala
+++ project/Build.scala
@@ -15,12 +15,24 @@ object Build extends sbt.Build
     lazy val common = library("common")
 
     private def library(path: String): Project = Project(path, file(path)).
-        settings(libraryDependencies ++= dependency.akka ++ Seq(
-            dependency.log4j,
+        settings(libraryDependencies ++= dependency.akka ++ dependency.log ++ Seq(
             dependency.netty,
             dependency.scalaArm,
-            dependency.scalaTest,
-            dependency.slf4j))
+            dependency.scalaTest))
 
-    private object dependency extends Dependencies
+    private object dependency {
+        lazy val akka = Seq(
+            "com.typesafe.akka" % "akka-actor_2.10" % "2.3.4",
+            "com.typesafe.akka" % "akka-slf4j_2.10" % "2.3.4",
+            "com.typesafe.akka" % "akka-testkit_2.10" % "2.3.4")
+        lazy val netty = "io.netty" % "netty-all" % "4.0.13.Final"
+        lazy val scalaArm = "com.jsuereth" % "scala-arm_2.10" % "1.4"
+        lazy val scalaTest = "org.scalatest" % "scalatest_2.10" % "2.2.1"
+        lazy val log = Seq(
+            "org.slf4j" % "slf4j-api" % "1.7.12",
+            "com.github.bespalovdn" % "scala-log" % "1.0-SNAPSHOT",
+            "org.slf4j" % "slf4j-log4j12" % "1.7.12" % "test",
+            "log4j" % "log4j" % "1.2.17" % "test"
+        )
+    }
 }
