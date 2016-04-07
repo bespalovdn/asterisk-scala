cce04f836541b3b04e9b509f1793081a2ccd3caa
Dmitry Bespalov <bespalovdn@gmail.com>
2016-04-07 22:57:01 +0300

    refactoring with ChannelStatus

--- agi/src/main/scala/com/github/bespalovdn/asteriskscala/agi/command/ChannelStatus.scala
+++ agi/src/main/scala/com/github/bespalovdn/asteriskscala/agi/command/ChannelStatus.scala
@@ -1,6 +1,6 @@
 package com.github.bespalovdn.asteriskscala.agi.command
 
-import com.github.bespalovdn.asteriskscala.agi.command.response.{AgiResponse, ChannelStatusResponse}
+import com.github.bespalovdn.asteriskscala.agi.command.response.{AgiResponse, CustomAgiResponse}
 import com.github.bespalovdn.asteriskscala.agi.execution.AsyncAction
 import com.github.bespalovdn.asteriskscala.agi.handler.AgiHandler
 import com.github.bespalovdn.asteriskscala.common.protocol.AsteriskFormatter
@@ -19,24 +19,36 @@ class ChannelStatus private (val channel: Option[String]) extends AgiCommand wit
         "CHANNEL STATUS" + channel.map(" " + _.escaped).getOrElse("")
     }
 
-    override def send()(implicit handler: AgiHandler): Future[ChannelStatusResponse] =
-        handler.send(this) map toResult
-
-    private def toResult(response: AgiResponse): ChannelStatusResponse = response.resultCode match {
-        case "0" => new ChannelStatusResponse.ChannelDownAndAvailable(response)
-        case "1" => new ChannelStatusResponse.ChannelDownButReserved(response)
-        case "2" => new ChannelStatusResponse.ChannelIsOffHook(response)
-        case "3" => new ChannelStatusResponse.DigitsBeenDialed(response)
-        case "4" => new ChannelStatusResponse.LineIsRinging(response)
-        case "5" => new ChannelStatusResponse.RemoteIsRinging(response)
-        case "6" => new ChannelStatusResponse.LineIsUp(response)
-        case "7" => new ChannelStatusResponse.LineIsBusy(response)
-        case other => throw new RuntimeException("Unexpected channel status: " + other)
-    }
+    override def send()(implicit handler: AgiHandler): Future[ChannelStatus.Response] =
+        handler.send(this).map{
+            case response => response.resultCode match {
+                case "0" => new ChannelStatus.Response.ChannelDownAndAvailable(response)
+                case "1" => new ChannelStatus.Response.ChannelDownButReserved(response)
+                case "2" => new ChannelStatus.Response.ChannelIsOffHook(response)
+                case "3" => new ChannelStatus.Response.DigitsBeenDialed(response)
+                case "4" => new ChannelStatus.Response.LineIsRinging(response)
+                case "5" => new ChannelStatus.Response.RemoteIsRinging(response)
+                case "6" => new ChannelStatus.Response.LineIsUp(response)
+                case "7" => new ChannelStatus.Response.LineIsBusy(response)
+                case other => throw new RuntimeException("Unexpected channel status: " + other)
+            }
+        }
 }
 
 object ChannelStatus
 {
     def apply() = new ChannelStatus(None)
     def apply(channel: String) = new ChannelStatus(Some(channel))
+
+    sealed trait Response extends CustomAgiResponse
+    object Response {
+        class ChannelDownAndAvailable(source: AgiResponse) extends Response
+        class ChannelDownButReserved(source: AgiResponse) extends Response
+        class ChannelIsOffHook(source: AgiResponse) extends Response
+        class DigitsBeenDialed(source: AgiResponse) extends Response
+        class LineIsRinging(source: AgiResponse) extends Response
+        class RemoteIsRinging(source: AgiResponse) extends Response
+        class LineIsUp(source: AgiResponse) extends Response
+        class LineIsBusy(source: AgiResponse) extends Response
+    }
 }
