917c91ad6294cf92917776cf8b6118d414ae2153
Dmitry Bespalov <bespalovdn@gmail.com>
2016-04-11 21:18:30 +0300

    AgiHandler.AgiCommandsOps impl

--- agi-examples/src/main/scala/com/github/bespalovdn/asteriskscala/examples/simpleagiserver/AgiHandlerImpl.scala
+++ agi-examples/src/main/scala/com/github/bespalovdn/asteriskscala/examples/simpleagiserver/AgiHandlerImpl.scala
@@ -1,19 +1,19 @@
 package com.github.bespalovdn.asteriskscala.examples.simpleagiserver
 
-import com.github.bespalovdn.asteriskscala.agi.command.{Hangup, Playback}
+import com.github.bespalovdn.asteriskscala.agi.command.{Answer, Hangup, Playback}
 import com.github.bespalovdn.asteriskscala.agi.handler.{AgiRequestHandler, AgiRequestHandlerFactory}
 import com.github.bespalovdn.asteriskscala.agi.request.AgiRequest
 
 import scala.concurrent.Future
 
-class AgiHandler extends AgiRequestHandler
+class AgiHandlerImpl extends AgiRequestHandler
 {
     override def handle(request: AgiRequest): Future[Unit] = {
-        Playback("demo-congrats").send() >> Hangup.send()
+        Seq(Answer, Playback("demo-congrats"), Hangup).send()
     }
 }
 
 class AgiHandlerFactory extends AgiRequestHandlerFactory
 {
-    override def createHandler(): AgiRequestHandler = new AgiHandler()
+    override def createHandler(): AgiRequestHandler = new AgiHandlerImpl()
 }
--- agi/src/main/scala/com/github/bespalovdn/asteriskscala/agi/handler/AgiHandler.scala
+++ agi/src/main/scala/com/github/bespalovdn/asteriskscala/agi/handler/AgiHandler.scala
@@ -2,10 +2,21 @@ package com.github.bespalovdn.asteriskscala.agi.handler
 
 import com.github.bespalovdn.asteriskscala.agi.command.AgiCommand
 import com.github.bespalovdn.asteriskscala.agi.command.response.AgiResponse
+import com.github.bespalovdn.asteriskscala.agi.execution.AsyncAction
 
 import scala.concurrent.Future
 
-trait AgiHandler
+trait AgiHandler extends AsyncAction
 {
     def send(command: AgiCommand): Future[AgiResponse]
+
+    implicit class AgiCommandsOps(commands: Traversable[AgiCommand])(implicit handler: AgiHandler) {
+        def send(): Future[AgiResponse] = {
+            val head = commands.head.send()
+            commands.tail.foldLeft(head){
+                case (response: Future[AgiResponse], command: AgiCommand) => response >> command.send()
+            }
+        }
+
+    }
 }
